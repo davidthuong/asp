504264157470457a942ba58d8d1dc24c9c068ed6
DavidThuong <huynh.thuong199x@gmail.com>
2022-07-14 15:34:32 +0700

    EF

---
+++ Eshopsolution.Data/EF/Eshopdbcontext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshopsolution.Data.EF
+{
+    public class Eshopdbcontext : DbContext
+
+    {
+        public Eshopdbcontext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+            options.UseSqlSever("");
+        }
+    }
+}
---
+++ Eshopsolution.Data/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshopsolution.Data.Entities
+{
+    class Product
+    {
+
+    }
+}
